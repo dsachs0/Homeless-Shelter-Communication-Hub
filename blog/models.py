c84b4bd6d76473922acf8359a50541c4fa6113fa
dsachs0 <150079614+dsachs0@users.noreply.github.com>
2024-02-03 23:29:42 -0800

    Commit 1
    This is the first commit of the Django blog website. This website includes user profiles, posting, updating, and deleting posts. This is just the first part of the website, there will be more features to come.

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['date_posted']
+
+    def __str__(self):
+        return 'Comment by {}'.format(self.author)
---
+++ users/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm, UserUpdateFrom, ProfileUpdateFrom
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+@login_required()
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateFrom(request.POST, instance=request.user)
+        p_form = ProfileUpdateFrom(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateFrom(instance=request.user)
+        p_form = ProfileUpdateFrom(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
+
+
+def my_logout(request):
+    logout(request)
+    return render(request, 'users/logout.html')
